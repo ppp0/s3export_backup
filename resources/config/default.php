7360275b4b17bb1c0950c682e0cb4b2d3b68cd96
Reto Kaiser <reto@cargomedia.ch>
2014-11-25 14:03:44 +0100

    Update CM

--- resources/config/default.php
+++ resources/config/default.php
@@ -1,8 +1,5 @@
 <?php
 
-$config->debug = true;
-
-$config->CM_Db_Db->db = 'cm-project';
-$config->CM_Db_Db->username = 'root';
-$config->CM_Db_Db->password = '';
-$config->CM_Db_Db->server = array('host' => '127.0.0.1', 'port' => 3306);
+return function (CM_Config_Node $config) {
+    $config->debug = true;
+};
