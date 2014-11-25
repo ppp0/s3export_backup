8a3bd596f197515cbc4d166aa85670059aa30949
Reto Kaiser <reto@cargomedia.ch>
2014-11-25 13:53:44 +0100

    Basic CM app

---
+++ resources/config/default.php
@@ -0,0 +1,8 @@
+<?php
+
+$config->debug = true;
+
+$config->CM_Db_Db->db = 'cm-project';
+$config->CM_Db_Db->username = 'root';
+$config->CM_Db_Db->password = '';
+$config->CM_Db_Db->server = array('host' => '127.0.0.1', 'port' => 3306);
