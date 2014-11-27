c715e171fedc14ce1a7f61bb353edae8070ecdc7
ppp0 <philipp.gysin@cargomedia.ch>
2014-11-27 19:47:34 +0100

    Implement mounting and decrypting, bump CM version, introduce VM for testing on target OS (linux)

--- library/S3Export/library/S3Export/Cli.php
+++ library/S3Export/library/S3Export/Cli.php
@@ -14,8 +14,28 @@ class S3Export_Cli extends CM_Cli_Runnable_Abstract implements CM_Service_Manage
      * @param string $truecryptPassword
      */
     public function verifyDisk($devicePath, $truecryptPassword) {
+
+        if ($jobId = $this->_mountDisk($devicePath)) {
+            $this->_decryptDisk($jobId, $truecryptPassword);
+        }
+        // randomly traverse decrypted partition and compare with S3
         $filesystemOriginal = $this->_getFilesystemOriginal();
-        $filesystemBackup = $this->_getFilesystemBackup();
+        $pathList = $filesystemOriginal->listByPrefix('photo/390', true);
+        foreach ($pathList as $path) {
+            foreach ($path as $entry) {
+                $fileOriginal = new CM_File($entry, $filesystemOriginal);
+                print $entry . "\n";
+                print $filesystemOriginal->getChecksum($entry) . " < -- etag \n";
+            }
+        }
+    }
+
+    public function initDisk($devicePath, $truecryptPassword) {
+        // initialize a disk to be ready to be sent to AWS
+    }
+
+    public function mountDisk($devicePath) {
+        var_dump($this->_mountDisk($devicePath));
     }
 
     /**
@@ -32,6 +52,22 @@ class S3Export_Cli extends CM_Cli_Runnable_Abstract implements CM_Service_Manage
      */
     private function _getFilesystemBackup() {
         return $this->getServiceManager()->get('s3export-filesystem-backup', 'CM_File_Filesystem');
+    }
+
+    /**
+     * @return CM_File_Filesystem
+     * @throws CM_Exception_Invalid
+     */
+    private function _getFilesystemEncrypted() {
+        return $this->getServiceManager()->get('s3export-filesystem-encrypted', 'CM_File_Filesystem');
+    }
+
+    /**
+     *
+     * @return string
+     */
+    private function _getPathPrefix(CM_File_Filesystem $filesystem) {
+        return $filesystem->getAdapter()->getPathPrefix();
     }
 
     public static function getPackageName() {
