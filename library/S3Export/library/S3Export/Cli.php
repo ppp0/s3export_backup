d4bc2c91825a724e19a30520442166be3e2c4119
ppp0 <philipp.gysin@cargomedia.ch>
2014-12-16 17:13:42 +0100

    Initial commit

--- library/S3Export/library/S3Export/Cli.php
+++ library/S3Export/library/S3Export/Cli.php
@@ -9,33 +9,101 @@ class S3Export_Cli extends CM_Cli_Runnable_Abstract implements CM_Service_Manage
         $this->setServiceManager(CM_Service_Manager::getInstance());
     }
 
+    public function __destruct() {
+        $this->_cleanup();
+    }
+
     /**
      * @param string $devicePath
      * @param string $truecryptPassword
      */
     public function verifyDisk($devicePath, $truecryptPassword) {
-
-        if ($jobId = $this->_mountDisk($devicePath)) {
-            $this->_decryptDisk($jobId, $truecryptPassword);
-        }
-        // randomly traverse decrypted partition and compare with S3
-        $filesystemOriginal = $this->_getFilesystemOriginal();
-        $pathList = $filesystemOriginal->listByPrefix('photo/390', true);
-        foreach ($pathList as $path) {
-            foreach ($path as $entry) {
-                $fileOriginal = new CM_File($entry, $filesystemOriginal);
-                print $entry . "\n";
-                print $filesystemOriginal->getChecksum($entry) . " < -- etag \n";
-            }
-        }
     }
 
-    public function initDisk($devicePath, $truecryptPassword) {
-        // initialize a disk to be ready to be sent to AWS
+    /**
+     *
+     */
+    public function initDisk($device) {
+        if (!preg_match('/\d+$/', $device)) {
+            CM_Util::exec('sgdisk', ['-o', $device]);
+            $startSector = CM_Util::exec('sgdisk', ['-F', $device]);
+            $endSector = CM_Util::exec('sgdisk', ['-E', $device]);
+            CM_Util::exec('sgdisk', ['-n', '1:' . $startSector . ':' . $endSector, $device]);
+            $device = $device . '1';
+        }
+//        CM_Util::exec('sudo mkfs', ['-t', 'ext4', '-m', '0', $device]);
+//        $mountpoint = $this->_getLocalFilesystemPath($this->_getFilesystemBackupEncrypted());
+//        CM_Util::exec('sudo mount', [$device, $mountpoint]);
+$mountpoint = '/tmp/test';
+
+        $file = new CM_File(getcwd() . '/manifest');
+        $manifest = $file->read();
+        if (!preg_match('/fileSystem:(.*)/', $manifest, $matches)) {
+            throw new CM_Exception_Invalid('Manifest file has not fileSystem field');
+        }
+        if (!$matches[1] == 'EXT4') {
+            throw new CM_Exception_Invalid('Only file system EXT4 supported (manifest)');
+        }
+
+        $apiResponse = $this->_createAWSJob($manifest, true);
+
+        $signatureFile = new CM_File($mountpoint . '/SIGNATURE');
+        $signatureFile->write($apiResponse->get('SignatureFileContents'));
+
+        print("\nCreate Job completed:\n");
+        print("---------------------\n");
+        print('JobID: ' . $apiResponse->get('JobId') . "\n");
+        print("\n\n");
+
     }
 
-    public function mountDisk($devicePath) {
-        var_dump($this->_mountDisk($devicePath));
+
+    public function cancelJob($jobId) {
+        $client = $this->_getAWSClient(CM_Config::get()->awsCredentials);
+        print_r($client->cancelJob(array(
+            'JobId' => $jobId,
+        )));
+    }
+
+    public function listJobs() {
+        $client = $this->_getAWSClient(CM_Config::get()->awsCredentials);
+        print_r($client->listJobs());
+    }
+
+    public function getStatus($jobId) {
+        $client = $this->_getAWSClient(CM_Config::get()->awsCredentials);
+        print_r($client->getStatus(array(
+            'JobId' => $jobId,
+        )));
+    }
+
+    /**
+     *
+     * @param string $manifest
+     * @param bool $dryRun
+     * @return String awsJobId
+     */
+    private function _createAWSJob($manifest, $dryRun = true) {
+        $client = $this->_getAWSClient(CM_Config::get()->awsCredentials);
+        $apiResponse = $client->createJob(array(
+            'JobType' => 'Export',
+            'Manifest' => $manifest,
+            'ValidateOnly' => $dryRun,
+        ));
+        return $apiResponse;
+    }
+
+    private function _getAWSClient($credentials) {
+        return Aws\ImportExport\ImportExportClient::factory($credentials);
+    }
+
+    private function _cleanup() {
+        try {
+            CM_Util::exec('sudo truecrypt', ['-d']);
+            CM_Util::exec('sudo umount', [$this->_getLocalFilesystemPath($this->_getFilesystemBackupEncrypted())]);
+        } catch (Exception $ignored) {
+            print $ignored->getMessage();
+        }
     }
 
     /**
