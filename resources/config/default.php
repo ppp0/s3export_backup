8b046c98e68e16f03955fd65623505ccbd148fbe
tomaszdurka <tomasz@durka.pl>
2015-01-15 11:08:20 +0100

    Remove unused service

--- resources/config/default.php
+++ resources/config/default.php
@@ -18,18 +18,6 @@ return function (CM_Config_Node $config) {
             ),
         ));
 
-    $config->services['s3export-filesystem-backup-encrypted'] = array(
-        'class'  => 'CM_File_Filesystem_Factory',
-        'method' => array(
-            'name'      => 'createFilesystem',
-            'arguments' => array(
-                'CM_File_Filesystem_Adapter_Local',
-                array(
-                    'pathPrefix' => '/media/s3disk_crypted',
-                )
-            ),
-        ));
-
     $config->services['s3export-backup-manager'] = [
         'class'     => 'S3Export_BackupManager',
         'arguments' => [
