d80a79978f71955677ccb3616ff43cec01a21a8d
tomaszdurka <tomasz@durka.pl>
2015-01-19 15:00:25 +0100

    Adjust puppetfile to puppet-cm changes, adjust manifest

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,21 +2,11 @@ node default {
 
   require 'truecrypt'
 
-  package {['gdisk']:}
-
-  class {'cm::application':
-  development => true,
+  class { 'cm::application':
+    development => true,
   }
 
-  environment::variable {'PHP_IDE_CONFIG':
-  value => 'serverName=www.cm.dev',
+  environment::variable { 'PHP_IDE_CONFIG':
+    value => 'serverName=www.s3export_backup.dev',
   }
-
-  file {['/media/s3disk_crypted', '/media/s3disk_decrypted']:
-      ensure => directory,
-      owner => '0',
-      group => '0',
-      mode => '0755',
-  }
-
 }
