b1d5bc2f5477f158797b79b9039e2aed8cb6aacf
Chris Stasiak <me@krzysztof-stasiak.com>
2016-08-24 18:38:31 +0100

    Updated env module name

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -7,7 +7,7 @@ node default {
     development => true,
   }
 
-  environment::variable { 'PHP_IDE_CONFIG':
+  env::variable { 'PHP_IDE_CONFIG':
     value => 'serverName=www.s3export_backup.dev',
   }
 }
