f77b0eacc6c1bb70ed0e7cd205da9c12f3f4e967
Reto Kaiser <reto@cargomedia.ch>
2016-11-18 17:23:51 +0100

    Don't install truecrypt

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,13 +1,9 @@
 node default {
 
-  require 'truecrypt'
   require 'gdisk'
 
   class { 'cm::application':
     development => true,
   }
 
-  env::variable { 'PHP_IDE_CONFIG':
-    value => 'serverName=www.s3export_backup.dev',
-  }
 }
